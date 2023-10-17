1d6c1969b2cad0a86d2a8370c0787b18bc91a336
Carlos Checa <carloschecaa@gmail.com>
2023-10-17 09:36:45 +0200

    get username from token

--- src/app.rs
+++ src/app.rs
@@ -7,6 +7,9 @@ use leptos::{
     leptos_dom::logging::{console_error, console_log},
     *,
 };
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -18,9 +21,7 @@ pub fn App() -> impl IntoView {
     let query_params = match params {
         crate::url_query::Query::Empty
         | crate::url_query::Query::Values(
-            QueryParams { channel: None, .. }
-            | QueryParams { token: None, .. }
-            | QueryParams { username: None, .. },
+            QueryParams { token: None, .. } | QueryParams { channel: None, .. },
         ) => {
             console_error("You have to set a channel, token and username");
             std::process::exit(1)
@@ -28,92 +29,128 @@ pub fn App() -> impl IntoView {
         crate::url_query::Query::Values(query_params) => query_params,
     };
 
-    let mut client = wasm_sockets::EventClient::new("wss://irc-ws.chat.twitch.tv:443")
-        .expect("Cannot connect to the server");
+    spawn_local(async move {
+        let username = {
+            if let Some(username) = &query_params.username {
+                username.clone()
+            } else {
+                let mut opts = RequestInit::new();
+                opts.method("GET");
+                opts.mode(RequestMode::Cors);
 
-    client.set_on_error(Some(Box::new(|error| {
-        console_log(format!("{:#?}", error).as_str());
-    })));
-
-    let channel = query_params.channel.as_ref().unwrap().clone();
-    client.set_on_connection(Some(Box::new(move |client: &wasm_sockets::EventClient| {
-        // console_log(format!("{:#?}", client.status).as_str());
-        // console_log(format!("Sending message...").as_str());
-        client
-            .send_string(
-                format!(
-                    "PASS oauth:{}",
-                    query_params.token.as_ref().unwrap().as_str()
-                )
-                .as_str(),
-            )
-            .unwrap();
-        client
-            .send_string("CAP REQ :twitch.tv/commands twitch.tv/tags")
-            .unwrap();
-        client
-            .send_string(
-                format!("NICK {}", query_params.username.as_ref().unwrap().as_str()).as_str(),
-            )
-            .unwrap();
-        client
-            .send_string(format!("JOIN #{}", channel).as_str())
-            .unwrap();
-    })));
-
-    client.set_on_close(Some(Box::new(|_evt| {
-        console_log(format!("Connection closed").as_str());
-    })));
-
-    client.set_on_message(Some(Box::new(
-        move |_client: &wasm_sockets::EventClient, message: wasm_sockets::Message| {
-            let msg = match message {
-                wasm_sockets::Message::Text(text) => text,
-                _ => "".to_owned(),
-            };
-
-            if msg.is_empty() || !msg.contains("PRIVMSG") {
-                return;
+                let url = "https://id.twitch.tv/oauth2/userinfo";
+                let request = Request::new_with_str_and_init(&url, &opts).unwrap();
+                request
+                    .headers()
+                    .set(
+                        "Authorization",
+                        format!("Bearer {}", query_params.token.as_ref().unwrap()).as_str(),
+                    )
+                    .unwrap();
+                let window = web_sys::window().unwrap();
+                let resp_value = JsFuture::from(window.fetch_with_request(&request))
+                    .await
+                    .unwrap();
+                let resp: Response = resp_value.dyn_into().unwrap();
+                let json = JsFuture::from(resp.json().unwrap()).await.unwrap();
+                serde_wasm_bindgen::from_value::<serde_json::Value>(json).unwrap()
+                    ["preferred_username"]
+                    .as_str()
+                    .unwrap_or("")
+                    .to_owned()
             }
+        };
 
-            let twitch_msg = crate::twitch::parse_twitch_message(&msg);
+        if username.is_empty() {
+            console_error("Error getting username");
+            std::process::exit(1)
+        }
 
-            // console_log(format!("New Message: {}", &msg).as_str());
+        let mut client = wasm_sockets::EventClient::new("wss://irc-ws.chat.twitch.tv:443")
+            .expect("Cannot connect to the server");
 
-            if let ChatTypeMessage::Message(twitch_message) = twitch_msg {
-                set_messages.update(|f| f.push_back(twitch_message));
+        client.set_on_error(Some(Box::new(|error| {
+            console_log(format!("{:#?}", error).as_str());
+        })));
 
-                let element = document().get_element_by_id("app").unwrap();
-                element.scroll_into_view_with_bool(false);
+        let channel = query_params.channel.as_ref().unwrap().clone();
+        client.set_on_connection(Some(Box::new(move |client: &wasm_sockets::EventClient| {
+            // console_log(format!("{:#?}", client.status).as_str());
+            // console_log(format!("Sending message...").as_str());
+            client
+                .send_string(
+                    format!(
+                        "PASS oauth:{}",
+                        query_params.token.as_ref().unwrap().as_str()
+                    )
+                    .as_str(),
+                )
+                .unwrap();
+            client
+                .send_string("CAP REQ :twitch.tv/commands twitch.tv/tags")
+                .unwrap();
+            client
+                .send_string(format!("NICK {}", username.as_str()).as_str())
+                .unwrap();
+            client
+                .send_string(format!("JOIN #{}", channel).as_str())
+                .unwrap();
+        })));
+
+        client.set_on_close(Some(Box::new(|_evt| {
+            console_log(format!("Connection closed").as_str());
+        })));
+
+        client.set_on_message(Some(Box::new(
+            move |_client: &wasm_sockets::EventClient, message: wasm_sockets::Message| {
+                let msg = match message {
+                    wasm_sockets::Message::Text(text) => text,
+                    _ => "".to_owned(),
+                };
+
+                if msg.is_empty() || !msg.contains("PRIVMSG") {
+                    return;
+                }
+
+                let twitch_msg = crate::twitch::parse_twitch_message(&msg);
+
+                // console_log(format!("New Message: {}", &msg).as_str());
+
+                if let ChatTypeMessage::Message(twitch_message) = twitch_msg {
+                    set_messages.update(|f| f.push_back(twitch_message));
+
+                    let element = document().get_element_by_id("app").unwrap();
+                    element.scroll_into_view_with_bool(false);
 
                     if messages.get_untracked().len() > 14 {
-                    set_messages.update(|f| {
-                        (0..6).for_each(|_| {
-                            f.pop_front();
+                        set_messages.update(|f| {
+                            (0..6).for_each(|_| {
+                                f.pop_front();
+                            });
                         });
-                    });
-                } else {
-                    // set_timeout(
-                    //     move || {
-                    //         if messages.get().len() > 0 {
-                    //             set_messages.update(|f| {
-                    //                 f.pop_front();
-                    //             });
-                    //         }
-                    //     },
-                    //     Duration::from_secs(10),
-                    // );
+                    } else {
+                        // set_timeout(
+                        //     move || {
+                        //         if messages.get().len() > 0 {
+                        //             set_messages.update(|f| {
+                        //                 f.pop_front();
+                        //             });
+                        //         }
+                        //     },
+                        //     Duration::from_secs(10),
+                        // );
+                    }
                 }
-            }
-        },
-    )));
+            },
+        )));
 
-    set_interval(
-        move || {
-            client.send_string("PING").unwrap();
-        },
-        Duration::from_secs(60),
-    );
+        set_interval(
+            move || {
+                client.send_string("PING").unwrap();
+            },
+            Duration::from_secs(60),
+        );
+    });
 
     view! {
         <div id="app">
